7c0f67b2e938c9955f038a5ceebdd9b3bd002c06
jean-eudes <jeudes7@gmail.com>
2025-12-06 22:01:57 +0100

    resolve 4 exo.

---
+++ exo4/src/part1.rs
@@ -0,0 +1,140 @@
+use std::cmp::PartialEq;
+use std::fmt::{Display, Formatter};
+use std::fs::read_to_string;
+
+#[derive(PartialEq)]
+enum Field {
+    EMPTY,
+    FULL,
+}
+
+impl Display for Field {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Field::EMPTY => write!(f, "."),
+            Field::FULL => write!(f, "@"),
+        }
+    }
+}
+
+struct Board {
+    width: i64,
+    height: i64,
+    fields: Vec<Field>,
+}
+
+impl Board {
+    fn new() -> Board {
+        Board {
+            width: 0,
+            height: 0,
+            fields: Vec::new(),
+        }
+    }
+
+    fn add(&mut self, c: char) {
+        self.fields.push(Field::from(c));
+    }
+
+    fn width(&mut self, width: i64) {
+        self.width = width
+    }
+    fn height(&mut self, width: i64) {
+        self.height = width
+    }
+
+    fn cell(&self, x: i64, y: i64) -> &Field {
+        if x >= self.width || y >= self.fields.len() as i64 / self.width || x < 0 || y < 0 {
+            return &Field::EMPTY;
+        };
+        &self.fields[x as usize + y as usize * self.width as usize]
+    }
+
+    fn neighbors(&self, x: i64, y: i64) -> usize {
+        let mut number_of_neighbors = 0;
+
+        if *self.cell(x - 1, y - 1) == Field::FULL {
+            number_of_neighbors += 1;
+        }
+        if *self.cell(x, y - 1) == Field::FULL {
+            number_of_neighbors += 1;
+        }
+        if *self.cell(x - 1, y) == Field::FULL {
+            number_of_neighbors += 1;
+        }
+        if *self.cell(x + 1, y) == Field::FULL {
+            number_of_neighbors += 1;
+        }
+        if *self.cell(x, y + 1) == Field::FULL {
+            number_of_neighbors += 1;
+        }
+        if *self.cell(x + 1, y + 1) == Field::FULL {
+            number_of_neighbors += 1;
+        }
+        if *self.cell(x - 1, y + 1) == Field::FULL {
+            number_of_neighbors += 1;
+        }
+        if *self.cell(x + 1, y - 1) == Field::FULL {
+            number_of_neighbors += 1;
+        }
+
+        number_of_neighbors
+    }
+
+    fn print(&self) {
+        let mut x = 0;
+        for field in self.fields.iter() {
+            print!("{}", field);
+            x += 1;
+            if x == self.width {
+                println!();
+                x = 0;
+            };
+        }
+    }
+}
+
+impl From<char> for Field {
+    fn from(c: char) -> Field {
+        match c {
+            '.' => Field::EMPTY,
+            '@' => Field::FULL,
+            _ => Field::EMPTY,
+        }
+    }
+}
+
+fn main() {
+    let mut board = Board::new();
+
+    let string = read_to_string("exo4/example/example2.txt").unwrap();
+
+    let mut height = 0;
+
+    for line in string.lines() {
+        height += 1;
+        board.width(line.len() as i64);
+        for x in line.chars() {
+            board.add(x);
+        }
+    }
+
+    board.height(height);
+
+    println!("{} {}", board.width, board.height);
+
+    let mut result = 0;
+
+    for y in 0..board.height  {
+        for x in 0..board.width  {
+            let i = board.neighbors(x, y);
+            if i < 4 && *board.cell(x, y) == Field::FULL {
+                result += 1;
+            }
+        }
+    }
+
+    board.print();
+
+    println!("result is {result:?}");
+}
