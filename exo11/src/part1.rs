b86223bd0ba29a6cf85d828458638e334a6898f6
jean-eudes <jeudes7@gmail.com>
2026-02-20 21:16:29 +0100

    refactor exo11: update Graph to use lifetime annotations and improve parsing logic

--- exo11/src/part1.rs
+++ exo11/src/part1.rs
@@ -1,17 +1,17 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
-struct Graph {
-    nodes: HashMap<String, Vec<String>>,
+struct Graph<'a> {
+    nodes: HashMap<&'a str, Vec<&'a str>>,
 }
 
-impl Graph {
+impl<'a> Graph<'a> {
     fn new() -> Self {
         let map = HashMap::new();
         Graph { nodes: map }
     }
 
-    fn add_node(&mut self, a: String, b: String) {
+    fn add_node(&mut self, a: &'a str, b: &'a str) {
         self.nodes.entry(a).or_insert_with(Vec::new).push(b);
     }
 }
@@ -21,11 +21,10 @@ fn main() {
     let mut graph = Graph::new();
 
     for line in string.lines() {
-        let root = line.split(":").next();
-        let edges_opt = line.split(":").skip(1).next();
-        let split = edges_opt.unwrap().trim().split(" ");
+        let (root, edges) = line.split_once(":").unwrap();
+        let split = edges.trim().split_whitespace();
         for edge in split {
-            graph.add_node(root.unwrap().to_string(), edge.to_string());
+            graph.add_node(root, edge);
         }
     }
 
@@ -39,7 +38,7 @@ fn traverse_graph(graph: &Graph, start_node: &str) -> usize {
     if let Some(edges) = graph.nodes.get(start_node) {
         let mut result = 0;
         for edge in edges {
-            if edge == "out" {
+            if *edge == "out" {
                 result += 1;
             }
             result += traverse_graph(graph, edge);
