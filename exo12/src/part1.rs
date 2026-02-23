8fedba32fb1bd8b58119793797a1faa95b86e298
jean-eudes <jeudes7@gmail.com>
2026-02-23 20:20:00 +0100

    add piece and board struct implementations with signature methods and initial file reading logic

--- exo12/src/part1.rs
+++ exo12/src/part1.rs
@@ -1,3 +1,52 @@
+use std::fs::read_to_string;
+
+struct Piece {
+    white: isize,
+    black: isize,
+}
+
+impl Piece {
+    fn new(white: isize, black: isize) -> Piece {
+        Piece { white, black }
+    }
+    fn signature(&self) -> isize {
+        self.black - self.white
+    }
+}
+
+struct Board {
+    width: usize,
+    height: usize,
+}
+
+impl Board {
+    fn new(width: usize, height: usize) -> Board {
+        Board { width, height }
+    }
+
+    fn signature(&self) -> isize {
+        if self.width * self.height % 2 == 0 {
+            0
+        } else {
+            1
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let string = read_to_string("exo12/example/example1.txt").unwrap();
+
+    let number_pieces = 5;
+    let counter_piece = 0;
+
+    for i in 0..5 {
+        let number_pieces = string.lines().count();
+    }
+
+    for line in string.lines() {
+        let (root, edges) = line.split_once(":").unwrap();
+        let split = edges.trim().split_whitespace();
+        for edge in split {
+        }
+    }
 }
